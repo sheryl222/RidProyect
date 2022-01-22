26a9f446b41ebc61467be23209b391c9f60eeff7
Pablo La Plata <p.laplata.m@gmail.com>
2022-01-21 22:03:38 -0500

    resubiendo todo

---
+++ RidProyect/Codigo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RidProyect
+//
+//  Created by Pablo La Plata on 20/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
